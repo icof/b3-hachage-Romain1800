58f64646f627d793d04be804185a6794f263d7a5
Romain1800 <romain.audebert@clsi.fr>
2026-04-27 16:32:55 +0000

    correction connexion BDD avec Docker

--- site/BDD/configBdd.php
+++ site/BDD/configBdd.php
@@ -1,5 +1,5 @@
 <?php
-$_ENV['MYSQL_HOST'] = 'localhost'; // le chemin vers le serveur ("db" en version docker, "localhost" en version serveur local)
+$_ENV['MYSQL_HOST'] = 'db'; // le chemin vers le serveur ("db" en version docker, "localhost" en version serveur local)
 $_ENV['MYSQL_PORT'] = '3306'; // le port de connexion à la base de données
 $_ENV['MYSQL_DATABASE'] = 'm2l_appli'; // le nom de votre base de données
 $_ENV['MYSQL_USER'] = 'user-mdl'; // nom d'utilisateur pour se connecter
